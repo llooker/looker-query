9ff89a12e753f263f38f962e4e3066ab11a5402e
Claude Perdigou <claude.perdigou@dataiku.com>
2019-10-10 22:51:56 +0200

    Update recipe to run a look

--- python-connectors/looker-query_run-query/connector.py
+++ python-connectors/looker-query_run-query/connector.py
@@ -1,79 +1,71 @@
 # This file is the actual code for the custom Python dataset looker-query_run-query
 
 from dataiku.connector import Connector
-from lookerapi import LookerApi
 
+import logging
 import pprint
+
+import json
+
+from looker_sdk import client, models, error
 
 class MyConnector(Connector):
 
     def __init__(self, config, plugin_config):
+        logging.basicConfig(level=logging.INFO, format='dss-plugin-looker-query %(levelname)s - %(message)s')
+        self.logger = logging.getLogger()
+
         Connector.__init__(self, config, plugin_config)  # pass the parameters to the base class
 
-        self.baseurl = self.config["baseurl"]
-        self.clientid = self.config["clientid"]
-        self.clientsecret = self.config["clientsecret"]
-        self.lookid = int(self.config["lookid"])
+        self.base_url = self.plugin_config["Looker API"]["baseurl"]
+        self.client_id = self.plugin_config["Looker API"]["clientid"]
+        self.client_secret = self.plugin_config["Looker API"]["clientsecret"]
+        self.look_id = int(self.config["lookid"])
         #self.limit = int(self.config["limit"])
-
-        if self.baseurl is None or self.clientid is None or self.clientsecret is None:
-            raise ValueError("Missing parameters (Base URL, or Client ID, or Client Secret")
         
+        print(self.plugin_config)
+
+        if not (self.base_url and self.client_id and self.client_secret):
+            self.logger.error('Connection params: {}'.format(
+                {'Client ID:' : self.client_id,
+                'Client secret:' : '#' * len(self.client_secret),
+                'Base URL:' : self.base_url})
+            )
+            raise ValueError("Client ID, Client secret and Base URL must be filled")
+
+        if not self.look_id:
+            raise ValueError("Look ID was not set")
+
         #if self.limit is None or self.limit = 0:
         #    self.limit = 500
-        
+
+        file = open("looker.ini", "w")
+        line_list = ["[Looker]", "api_version=3.1", "base_url=" + self.base_url, "client_id=" + self.client_id, "client_secret=" + self.client_secret, "verify_ssl=True"]
+        file.write('\n'.join(line_list) + '\n')
+        file.close()
+
+        self.looker_client = client.setup("looker.ini")
+        print(self.looker_client)
+
     def get_read_schema(self):
         return None
 
     def generate_rows(self, dataset_schema=None, dataset_partitioning=None,
                             partition_id=None, records_limit = -1):
 
-        looker = LookerApi(host=self.baseurl, token=self.clientid, secret = self.clientsecret)
-
-        data = looker.get_look(self.lookid, "json", limit=100000)
-
+        response = self.looker_client.run_look(self.look_id, "json", limit=records_limit)
+        data = json.loads(response)
+        
         for row in data:
             yield row
-        
-        #print(data)
 
-        ### ------- Done -------
-
-        #print("Done")
-        
-        """
-        looping = True
-        offset = None
-        params = {
-            "pageSize": 100
-        }
-
-        # if sort_by is not None:
-        #     params.update({
-        #         "sort[0][field]": sort_by,
-        #         "sort[0][direction]": sort_order}
-        #     )
-
-        while looping:
-            if offset != None:
-                params.update({'offset':offset})
-            results = airtable_api(self.base, self.table, self.key, parameters=params)
-            for record in results.get("records"):
-                if self.retrieve_id == 'yes':
-                    record["fields"]["id"] = record["id"]
-                yield record["fields"]
-            offset = results.get("offset")
-            looping = False if offset is None else True
-
-        """
-        
     def get_writer(self, dataset_schema=None, dataset_partitioning=None,
                          partition_id=None):
-        raise Exception("Unimplemented")
+        raise NotImplementedError("Writer")
 
 
     def get_partitioning(self):
-        raise Exception("Unimplemented")
+        raise NotImplementedError("Partition list")
 
 
     def list_partitions(self, partitioning):
@@ -81,10 +73,8 @@ class MyConnector(Connector):
 
 
     def partition_exists(self, partitioning, partition_id):
-        raise Exception("Unimplemented")
+        raise NotImplementedError("Partition check")
 
 
     def get_records_count(self, partitioning=None, partition_id=None):
-        raise Exception("Unimplemented")
-
-
+        raise NotImplementedError("Count records")
